fb3d17d82b6c426006b159f053dbce660480b09d
Alexey Ershov <alex4rom@gmail.com>
2016-11-19 10:41:56 +0300

    Add validation

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,8 +1,9 @@
 class Word < ActiveRecord::Base
   validates :word, presence: true
+  validates :kind, inclusion: { in: %w(:noun :adjective) }
 
-  scope :nouns, -> { where(kind: 'noun') }
-  scope :adjectives, -> { where(kind: 'adjective') }
+  scope :nouns, -> { where(kind: :noun) }
+  scope :adjectives, -> { where(kind: :adjective) }
 
   def as_json(options={})
     self.word
