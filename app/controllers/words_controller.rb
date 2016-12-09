12976e913671c6dae714e66576fd3b3664e5b7b2
Alexey Ershov <alex4rom@gmail.com>
2016-12-09 21:40:40 +0300

    Add 'edit'

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,7 @@
 class WordsController < ApplicationController
 
   before_action :find_words, only: [:index, :all_words]
+  before_action :find_word, only: [:edit, :update]
 
   def index
   end
@@ -19,10 +20,18 @@ class WordsController < ApplicationController
   def edit
   end
 
+  def update
+    if @word.update_attributes(word_params)
+      redirect_to action: :all_words
+    else
+      render action: :edit
+    end
+  end
+
   def create
     @word = Word.new(word: params[:word][:word], kind: params[:word][:kind])
     if @word.save
-      redirect_to action: 'index'
+      redirect_to action: :index
     else
       flash[:error] = @word.errors.full_messages
       redirect_to :back
@@ -37,11 +46,15 @@ class WordsController < ApplicationController
   private
 
   def word_params
-    params.require(:word).permit(:noun, :adjective)
+    params.require(:word).permit(:noun, :adjective, :kind)
   end
 
   def find_words
     @nouns = Word.nouns
     @adjectives = Word.adjectives
+  end
+
+  def find_word
+    @word = Word.find(params[:id])
   end
 end
